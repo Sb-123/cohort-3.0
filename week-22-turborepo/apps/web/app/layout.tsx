3fcc968c3e59b56e6d44ea9ab148e025e8160156
Turbobot <turbobot@vercel.com>
2025-01-23 17:21:53 +0530

    feat(create-turbo): apply official-starter transform

--- week-22-turborepo/apps/web/app/layout.tsx
+++ week-22-turborepo/apps/web/app/layout.tsx
@@ -1,15 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
-  subsets: ["latin"],
 });
-
-const geistMono = Geist_Mono({
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
-  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -24,14 +23,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <div className="bg-indigo-400">nav bar</div>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
-
-        <div className="bg-indigo-400 h-1/2">footer</div>
       </body>
     </html>
   );
-}  
+}
